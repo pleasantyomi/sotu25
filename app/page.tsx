c02d0d6a6a9fd96c5af491c74170060bf2aa27ba
Pleasant Yomi <110311997+pleasantyomi@users.noreply.github.com>
2024-11-13 23:11:31 +0100

    edit

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
 export default function Home() {
   return (
     <div className="h-screen grid items-center place-items-center">
-      <h1 className="text-center font-semibold lg:text-[6rem] text-[4.5rem]">SOTU'24</h1>
+      <h1 className="text-center font-semibold lg:text-[6rem] text-[4.5rem]">
+        SOTU24
+      </h1>
     </div>
   );
 }
